996450cee13f78d6998f11e20f2c41c2e31150e3
Lucas-OF28 <luuu_of@hotmail.com>
2022-08-09 16:00:08 -0300

    criação da base do projeto

---
+++ README.md
@@ -0,0 +1,9 @@
+# Projeto desenvolvido no curso de Node.js do Zero a Maestria com diversos Projetos.
+
+# Desenvolvimento de uma aplicação de banco, com opções de:
+
+* Criar conta
+* Consultar saldo
+* Depositar
+* Sacar
+* Sair.
--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-testeadadasd
+// modulos externos
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+// modulos internos
+
+const fs = require ('fs')
+
+operation()
+
+function operation (){
+  inquirer.prompt([
+    {
+    type: 'list',
+    name: 'action',
+    message: 'O que você deseja fazer?',
+    choices: ['Criar conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair']
+    },
+  ])
+  .then((answer) =>{
+
+    const action = answer['action']
+
+    if(action == 'Criar conta'){
+      createAccount()
+    }
+  })
+  .catch((err) => console.log(err))
+}
+
+// create an account
+
+function createAccount(){
+  console.log(chalk.bgGreen.black("Parabéns por escolher o nosso banco!"))
+  console.log(chalk.green("Defina as opções da sua conta a seguir"))
+}
